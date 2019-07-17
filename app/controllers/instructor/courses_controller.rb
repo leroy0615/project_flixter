099251c60080dc1e9231d6c8c9072c21510bad57
leroy0615 <leroy0615@gmail.com>
2019-07-17 12:49:35 +0000

    Video Embedding

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -8,6 +8,7 @@ class Instructor::CoursesController < ApplicationController
 
   def create
     @course = current_user.courses.create(course_params)
+    
     if @course.valid?
       redirect_to instructor_course_path(@course)
     else
