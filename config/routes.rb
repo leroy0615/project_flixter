39887acfc78bc3d077a94cb1905d50d71bf8d415
leroy0615 <leroy0615@gmail.com>
2019-07-23 11:09:33 +0000

    Lesson Drag & Drop

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+   devise_for :users
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
-
   resources :lessons, only: [:show]
   namespace :instructor do
+    resources :lessons, only: [:update]
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
